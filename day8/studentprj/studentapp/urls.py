b0024afa6e6026e2692faa7e19da362994e54d9c
Shuvo Kumar Shill <11577894+shuvo-kumar@users.noreply.github.com>
2021-11-11 20:03:39 +0600

    Day8 folder Create

---
+++ day8/studentprj/studentapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from studentapp import views
+
+
+urlpatterns = [
+    path('list/', views.list,name = 'list'),
+    path('about/', views.about,name = 'about'),
+    
+]

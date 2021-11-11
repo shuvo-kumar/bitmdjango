772d3dff4bd2ba99cb81a5ecc8a4bccbe23a216d
Shuvo Kumar Shill <11577894+shuvo-kumar@users.noreply.github.com>
2021-11-11 21:18:13 +0600

    Paginigation

--- day8/studentprj/studentapp/views.py
+++ day8/studentprj/studentapp/views.py
@@ -1,6 +1,9 @@
+from django.core import paginator
 from django.shortcuts import render
 
 from studentapp.models import Student
+from django.core.paginator import Paginator
+
 
 # Create your views here.
 def home(request):
@@ -8,8 +11,15 @@ def home(request):
     return render(request, 'stud/home.html', context)
 
 def list(request):
-    students = Student.objects.order_by('-id')
-    context = {'title ' : 'Student List', 'students': students}
+    # std = Student.objects.order_by('-id')
+    std = Student.objects.all()
+    std_list = std
+    paginator = Paginator(std_list,10)
+    
+    page = request.GET.get('page')
+    std = paginator.get_page(page)
+    
+    context = {'title ' : 'Student List', 'students': std}
     return render(request, 'stud/list.html',context)
 
     

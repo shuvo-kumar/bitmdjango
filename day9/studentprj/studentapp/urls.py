b717eec66854ffafdff5e5db424cf165494c1489
Shuvo Kumar Shill <11577894+shuvo-kumar@users.noreply.github.com>
2021-11-14 21:29:52 +0600

    Day9-CreateStudent -Completed

--- day9/studentprj/studentapp/urls.py
+++ day9/studentprj/studentapp/urls.py
@@ -5,5 +5,7 @@ from studentapp import views
 urlpatterns = [
     path('list/', views.list,name = 'list'),
     path('about/', views.about,name = 'about'),
+    path('createnew/', views.create,name = 'create'),
+    
     
 ]
--- day9/studentprj/studentapp/views.py
+++ day9/studentprj/studentapp/views.py
@@ -1,8 +1,12 @@
+import django
 from django.core import paginator
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from studentapp.models import Student
 from django.core.paginator import Paginator
+
+from .forms import StudentForm
+from django.contrib import messages
 
 
 # Create your views here.
@@ -11,8 +15,8 @@ def home(request):
     return render(request, 'stud/home.html', context)
 
 def list(request):
-    # std = Student.objects.order_by('-id')
-    std = Student.objects.all()
+    std = Student.objects.order_by('-id')
+    # std = Student.objects.all()
     std_list = std
     paginator = Paginator(std_list,10)
     
@@ -26,3 +30,19 @@ def list(request):
 def about(request):
     context = {'title': 'About'}
     return render(request, 'stud/about.html', context)
+
+def create(request):
+    # POST =when submit 
+    # GET =befor submit
+    if request.method == "POST":
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,'Data Inserted Succesfully !')
+            return redirect('list')
+    else :
+        form = StudentForm()     
+        
+    context = {'title' : 'Create Student', 'form':form}
+    return render(request, 'stud/create.html',context,)
+    

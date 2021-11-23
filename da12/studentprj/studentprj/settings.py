9b434da32c75363c40e06eec1fc6508fce56ef29
Shuvo Kumar Shill <11577894+shuvo-kumar@users.noreply.github.com>
2021-11-23 20:37:30 +0600

    Day12 1.utils email config

--- da12/studentprj/studentprj/settings.py
+++ da12/studentprj/studentprj/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'studentapp',
+    'utils',
 ]
 
 MIDDLEWARE = [
@@ -131,4 +132,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'bitmbatch1@gmail.com'
+EMAIL_HOST_PASSWORD ='@@@Error1234'
 
